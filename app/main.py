b2ea2c5da6f93aea25505ba1038998791e621883
Anna Samar <annsummmer@gmail.com>
2026-07-26 21:06:54 +0200

    added move file task solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3:
+        return
+    command_name, file_to_move, path = parts
+
+    if command_name != "mv":
+        return
+
+    if not os.path.isfile(file_to_move):
+        return
+
+    folder_path = os.path.dirname(path)
+
+    if folder_path:
+        os.makedirs(folder_path, exist_ok=True)
+
+    os.rename(file_to_move, path)
